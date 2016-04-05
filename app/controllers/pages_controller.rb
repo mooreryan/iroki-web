d950215609566e7d99cf6a52debc42b5b9b94d1e
Ryan Moore <moorer@udel.edu>
2016-04-05 16:11:37 -0400

    Run iroki on uploaded files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,20 +18,28 @@ class PagesController < ApplicationController
       @color_map_contents = File.open(@color_map_f).read
       @name_map_contents = File.open(@name_map_f).read
 
+
+      time_now = Time.now.strftime("%Y%m%d%H%M%S%L")
+      outf =
+        Rails.root.join("public", "uploads", "haha.#{time_now}.nex")
+
+      IrokiLib::Main::main(color_branches: @color_branches,
+                           color_taxa_names: @color_taxa_names,
+                           exact: @exact,
+                           remove_bootstraps_below: @remove_below.to_f,
+                           color_map_f: @color_map_f,
+                           name_map_f: @name_map_f,
+                           auto_color: nil,
+                           display_auto_color_options: nil,
+                           newick_f: @newick_f,
+                           out_f: outf)
+
+      @outf_contents = File.open(outf).read
+
       ApplicationHelper::remove_upload @newick_f
       ApplicationHelper::remove_upload @color_map_f
       ApplicationHelper::remove_upload @name_map_f
-
-      # IrokiLib::Main::main(color_branches: @color_branches,
-      #                      color_taxa_names: @color_taxa_names,
-      #                      exact: @exact,
-      #                      remove_bootstraps_below: @remove_below,
-      #                      color_map_f: @color_map,
-      #                      name_map_f: @name_map,
-      #                      auto_color: nil,
-      #                      display_auto_color_options: nil,
-      #                      newick_f: @newick,
-      #                      out_f: "haha.nex")
+      ApplicationHelper::remove_upload outf
 
     end
   end
