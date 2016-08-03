93a8f87cb574ebd782c8febcf2f9e634f9d90030
Ryan Moore <moorer@udel.edu>
2016-08-03 13:34:09 -0400

    Add specs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,36 +3,47 @@ class PagesController < ApplicationController
   end
 
   def submit
-    if params[:newick] && params[:color_map] && params[:name_map]
-      @newick = params[:newick]
-      @color_map = params[:color_map]
-      @name_map = params[:name_map]
-      @color_branches = params[:color_branches]
-      @color_taxa_names = params[:color_taxa_names]
-      @exact = params[:exact]
-      @remove_below = params[:remove_below]
+    @newick           = params[:newick_file]
+    @color_map        = params[:color_map]
+    @name_map         = params[:name_map]
+    @biom_file        = params[:biom_file]
+    @color_branches   = params[:color_branches]
+    @color_labels     = params[:color_labels]
+    @exact            = params[:exact]
+    @remove_below     = params[:remove_below]
+    @single_color     = params[:single_color]
+    @auto_color       = params[:auto_color]
 
-      basein = File.basename(@newick.original_filename,
-                             File.extname(@newick.original_filename))
+    # p [:debug_params, params]
 
-      outf =
-        Tempfile.new ["#{basein}.", ".nex"]
+    basein = File.basename(@newick.original_filename,
+                           File.extname(@newick.original_filename))
 
-      begin
-        IrokiLib::Main::main(color_branches: @color_branches,
-                             color_taxa_names: @color_taxa_names,
-                             exact: @exact,
-                             remove_bootstraps_below: @remove_below.to_f,
-                             color_map_f: @color_map.tempfile.path,
-                             name_map_f: @name_map.tempfile.path,
-                             auto_color: nil,
-                             display_auto_color_options: nil,
-                             newick_f: @newick.tempfile.path,
-                             out_f: outf.path)
-        send_file outf.path, type: "text"
-      rescue AbortIf::Exit => e
-        @apple ||= e.message
-      end
+    newick_path    = @newick.tempfile.path if @newick
+    color_map_path = @color_map.tempfile.path if @color_map
+    name_map_path  = @name_map.tempfile.path if @name_map
+    biom_file_path = @biom_file.tempfile.path if @biom_file
+
+    outf =
+      Tempfile.new ["#{basein}.", ".nex"]
+
+    begin
+      Iroki::Main::main(color_branches: @color_branches,
+                        color_taxa_names: @color_labels,
+                        exact: @exact,
+                        remove_bootstraps_below: @remove_below.to_f,
+                        color_map_f: color_map_path,
+                        biom_f: biom_file_path,
+                        single_color: @single_color,
+                        name_map_f: name_map_path,
+                        auto_color: @auto_color,
+                        display_auto_color_options: nil,
+                        newick_f: newick_path,
+                        out_f: outf.path)
+
+      send_file outf.path, type: "text"
+    rescue AbortIf::Exit => e
+      @apple ||= e.message
     end
   end
 
--- spec/features/user_submits_a_tree_spec.rb
+++ spec/features/user_submits_a_tree_spec.rb
@@ -1,13 +1,112 @@
 require 'rails_helper'
 
 feature 'User submits a tree' do
-  scenario 'they see the home page' do
-    tree = "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);"
+  let(:test_files) { File.join File.dirname(__FILE__), "test_files" }
+  let(:basic_tre) { File.join test_files, "basic.tre" }
+
+  let(:basic_color_map_with_tags) {
+    File.join test_files, "basic_color_map_with_tags.txt" }
+  let(:basic_color_map_regex) {
+    File.join test_files, "basic_color_map_regex.txt" }
+
+  let(:basic_branches) {
+    File.join test_files, "basic_branches_only.nex" }
+  let(:basic_branches_regex) {
+    File.join test_files, "basic_branches_only_regex.nex" }
+
+  let(:basic_labels) {
+    File.join test_files, "basic_labels_only.nex"}
+  let(:basic_labels_regex) {
+    File.join test_files, "basic_labels_only_regex.nex"}
+
+  let(:basic_labels_and_branches) {
+    File.join test_files, "basic_labels_and_branches.nex"}
+  let(:basic_labels_and_branches_regex) {
+    File.join test_files, "basic_labels_and_branches_regex.nex"}
+
+  scenario "they want to color branches with exact matching" do
     visit root_path
 
-    fill_in 'Newick', with: tree
-    click_button 'Submit'
+    attach_file :newick_file, basic_tre
+    attach_file :color_map, basic_color_map_with_tags
+    check :color_branches
+    check :exact
 
-    expect(page).to have_content tree
+    click_button "Submit"
+
+    expect_the_downloaded_nexus_file_to_be basic_branches
   end
+
+  scenario "they want to color branches with regex matching" do
+    visit root_path
+
+    attach_file :newick_file, basic_tre
+    attach_file :color_map, basic_color_map_regex
+    check :color_branches
+
+    click_button "Submit"
+
+    expect_the_downloaded_nexus_file_to_be basic_branches_regex
+  end
+
+  scenario "they want to color labels with exact matching" do
+    visit root_path
+
+    attach_file :newick_file, basic_tre
+    attach_file :color_map, basic_color_map_with_tags
+    check :color_labels
+    check :exact
+
+    click_button "Submit"
+
+    expect_the_downloaded_nexus_file_to_be basic_labels
+  end
+
+  scenario "they want to color labels with regex matching" do
+    visit root_path
+
+    attach_file :newick_file, basic_tre
+    attach_file :color_map, basic_color_map_regex
+    check :color_labels
+
+    click_button "Submit"
+
+    expect_the_downloaded_nexus_file_to_be basic_labels_regex
+  end
+
+  scenario "they want to color labels and branches with exact matching" do
+    visit root_path
+
+    attach_file :newick_file, basic_tre
+    attach_file :color_map, basic_color_map_with_tags
+    check :color_labels
+    check :color_branches
+    check :exact
+
+    click_button "Submit"
+
+    expect_the_downloaded_nexus_file_to_be basic_labels_and_branches
+  end
+
+
+  scenario "they want to color labels and branches with regex matching" do
+    visit root_path
+
+    attach_file :newick_file, basic_tre
+    attach_file :color_map, basic_color_map_regex
+    check :color_labels
+    check :color_branches
+
+    click_button "Submit"
+
+    expect_the_downloaded_nexus_file_to_be basic_labels_and_branches_regex
+  end
+end
+
+# see http://collectiveidea.com/blog/archives/2012/01/27/testing-file-downloads-with-capybara-and-chromedriver/
+def expect_the_downloaded_nexus_file_to_be content
+  expect(page.response_headers["Content-Disposition"]).
+    to have_content "attachment"
+
+  expect(page.source).to eq File.open(content).read
 end
